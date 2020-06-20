5e981951bbef1ab1311349ab893b8e83899078db
Derek Etman <dereketman@gmail.com>
2020-06-19 22:39:15 -0700

    + fixed error

---
+++ src/containers/CardContainer.js
@@ -0,0 +1,11 @@
+import { connect } from 'react-redux';
+import Card from '../components/Card';
+
+const mapStateToProps = (state, ownProps) => {
+    console.log(state)
+  return {
+    card: state.cards.entities[ownProps.cardId],
+  };
+};
+
+export default connect(mapStateToProps)(Card);
